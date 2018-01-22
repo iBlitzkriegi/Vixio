3568962cd78c836f7231f32f756fe012aaa68d17
Pikachu920 <e@mail.com>
2018-01-22 15:12:06 -0600

    start work on commadns

--- src/main/java/me/iblitzkriegi/vixio/commands/DiscordCommandEvent.java
+++ src/main/java/me/iblitzkriegi/vixio/commands/DiscordCommandEvent.java
@@ -1,6 +1,9 @@
 package me.iblitzkriegi.vixio.commands;
 
+import ch.njol.skript.config.Config;
+import ch.njol.skript.log.RetainingLogHandler;
 import ch.njol.skript.log.SkriptLogger;
+import me.iblitzkriegi.vixio.Vixio;
 import org.bukkit.event.Event;
 import org.eclipse.jdt.annotation.Nullable;
 
@@ -15,27 +18,38 @@ import ch.njol.skript.lang.Trigger;
  * @author Peter Güttinger
  */
 public class DiscordCommandEvent extends SelfRegisteringSkriptEvent {
+
+    private RetainingLogHandler handler;
+
     static {
-        Skript.registerEvent("Script Load/Unload", DiscordCommandEvent.class, ScriptEvent.class, "discord command <.*>");
+        Vixio.getInstance().registerEvent("Discord Command", DiscordCommandEvent.class, null, "discord command <.*>");
     }
 
     @Override
     public boolean init(final Literal<?>[] args, final int matchedPattern, final ParseResult parser) {
         System.out.println("Node is " + SkriptLogger.getNode());
+        handler = SkriptLogger.startRetainingLog();
+        System.out.println("Started log " + handler);
         return true;
     }
 
     @Override
-    public void register(final Trigger t) {
+    public void afterParse(Config config) {
+        System.out.println("stopping " + handler);
+        handler.clear();
+        handler.printErrors();
+        handler.stop();
+        System.out.println("stopped " + handler);
     }
 
     @Override
-    public void unregister(final Trigger t) {
-    }
+    public void register(final Trigger t) {}
 
     @Override
-    public void unregisterAll() {
-    }
+    public void unregister(final Trigger t) {}
+
+    @Override
+    public void unregisterAll() {}
 
     @Override
     public String toString(final @Nullable Event e, final boolean debug) {
