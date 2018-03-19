a0e077969dd63a7a1269c64603c572db6d780349
iBlitzkriegi <Blitz>
2018-03-19 15:27:37 -0400

    Version bump and fixed mention tag

--- src/main/java/me/iblitzkriegi/vixio/expressions/ExprMentionTag.java
+++ src/main/java/me/iblitzkriegi/vixio/expressions/ExprMentionTag.java
@@ -2,17 +2,20 @@ package me.iblitzkriegi.vixio.expressions;
 
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import me.iblitzkriegi.vixio.Vixio;
-import me.iblitzkriegi.vixio.util.wrapper.Emote;
+import net.dv8tion.jda.core.entities.Channel;
+import net.dv8tion.jda.core.entities.ChannelType;
 import net.dv8tion.jda.core.entities.IMentionable;
+import net.dv8tion.jda.core.entities.TextChannel;
 
 /**
  * Created by Blitz on 7/25/2017.
  */
 public class ExprMentionTag extends SimplePropertyExpression<Object, String> {
     static {
-        Vixio.getInstance().registerPropertyExpression(ExprMentionTag.class, String.class, "mention tag", "users/textchannels/members/emotes/roles")
+        Vixio.getInstance().registerPropertyExpression(ExprMentionTag.class, String.class, "mention tag", "users/channel/members/emotes/roles")
                 .setName("Mention of")
                 .setDesc("Get the mention tag of something")
+                .setUserFacing("users/textchannels/members/emotes/roles")
                 .setExample("reply with mention tag of event-user");
     }
 
@@ -23,6 +26,13 @@ public class ExprMentionTag extends SimplePropertyExpression<Object, String> {
 
     @Override
     public String convert(Object o) {
+        if (o instanceof Channel) {
+            if (!(((Channel) o).getType() == ChannelType.TEXT)) {
+                return null;
+            }
+            return ((TextChannel) o).getAsMention();
+
+        }
         return ((IMentionable) o).getAsMention();
     }
 
