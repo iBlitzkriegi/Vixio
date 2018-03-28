0c816abd3255bf1cf30431d2452a02da389a1533
iBlitzkriegi <Blitz>
2018-03-28 00:13:57 -0400

    Added url of track

---
+++ src/main/java/me/iblitzkriegi/vixio/expressions/track/ExprUrl.java
@@ -0,0 +1,30 @@
+package me.iblitzkriegi.vixio.expressions.track;
+
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import me.iblitzkriegi.vixio.Vixio;
+
+public class ExprUrl extends SimplePropertyExpression<AudioTrack, String> {
+    static {
+        Vixio.getInstance().registerPropertyExpression(ExprUrl.class, String.class,
+                "url", "tracks")
+                .setName("Url of track")
+                .setDesc("Get a track's url.")
+                .setExample("broadcast \"%url of track event-bot is playing%\"");
+    }
+
+    @Override
+    protected String getPropertyName() {
+        return "url";
+    }
+
+    @Override
+    public String convert(AudioTrack audioTrack) {
+        return audioTrack.getInfo().uri;
+    }
+
+    @Override
+    public Class<? extends String> getReturnType() {
+        return String.class;
+    }
+}
