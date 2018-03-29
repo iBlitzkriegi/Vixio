18d806d7da524229630d230529915f0ee64f1fdb
iBlitzkriegi <Blitz>
2018-03-29 01:37:52 -0400

    Added track start event

--- src/main/java/me/iblitzkriegi/vixio/util/audio/TrackScheduler.java
+++ src/main/java/me/iblitzkriegi/vixio/util/audio/TrackScheduler.java
@@ -5,6 +5,8 @@ import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
 import me.iblitzkriegi.vixio.Vixio;
+import me.iblitzkriegi.vixio.events.base.TrackEvent;
+import org.bukkit.Bukkit;
 
 import java.util.ArrayList;
 import java.util.concurrent.BlockingQueue;
@@ -46,7 +48,10 @@ public class TrackScheduler extends AudioEventAdapter {
     }
     @Override
     public void onTrackStart(AudioPlayer player, AudioTrack track) {
-        //TODO add start event
+        MusicStorage musicStorage = Vixio.getInstance().musicStorage.get(player);
+        if (musicStorage != null) {
+            Bukkit.getPluginManager().callEvent(new TrackEvent(TrackEvent.TrackState.START, musicStorage.getBot(), musicStorage.getGuild(), track));
+        }
     }
     public ArrayList getQueue() {
         return new ArrayList<>(queue);
