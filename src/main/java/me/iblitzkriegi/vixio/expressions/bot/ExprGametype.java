d1a704ec3a009bec007e2b1932f2d339d9c231b0
iBlitzkriegi <Blitz>
2018-04-18 17:59:47 -0400

    Add game type of expression

---
+++ src/main/java/me/iblitzkriegi/vixio/expressions/bot/ExprGametype.java
@@ -0,0 +1,41 @@
+package me.iblitzkriegi.vixio.expressions.bot;
+
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import me.iblitzkriegi.vixio.Vixio;
+import me.iblitzkriegi.vixio.util.Util;
+import me.iblitzkriegi.vixio.util.wrapper.Bot;
+import net.dv8tion.jda.core.entities.Game;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.User;
+
+public class ExprGametype extends SimplePropertyExpression<Object, Game.GameType> {
+    static {
+        Vixio.getInstance().registerPropertyExpression(ExprGametype.class, Game.GameType.class, "game type", "bots/users/strings");
+    }
+    @Override
+    protected String getPropertyName() {
+        return "game type";
+    }
+
+    @Override
+    public Game.GameType convert(Object object) {
+        if (object instanceof Bot || object instanceof String) {
+            Bot bot = Util.botFrom(object);
+            if (bot != null) {
+                return bot.getJDA().getPresence().getGame().getType();
+            }
+        } else if (object instanceof User) {
+            Member member = Util.getMemberFromUser(object);
+            if (member != null) {
+                return member.getGame() == null ? null : member.getGame().getType();
+            }
+            return null;
+        }
+        return null;
+    }
+
+    @Override
+    public Class<? extends Game.GameType> getReturnType() {
+        return Game.GameType.class;
+    }
+}
