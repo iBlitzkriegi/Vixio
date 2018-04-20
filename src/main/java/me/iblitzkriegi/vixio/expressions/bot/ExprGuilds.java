ce6c179cbf3be77b31e8c542121fc9acbb586f76
iBlitzkriegi <Blitz>
2018-04-19 20:08:41 -0400

    Fixed guilds of bot

--- src/main/java/me/iblitzkriegi/vixio/expressions/bot/ExprGuilds.java
+++ src/main/java/me/iblitzkriegi/vixio/expressions/bot/ExprGuilds.java
@@ -1,29 +1,41 @@
 package me.iblitzkriegi.vixio.expressions.bot;
 
-import ch.njol.skript.expressions.base.PropertyExpression;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
 import me.iblitzkriegi.vixio.Vixio;
+import me.iblitzkriegi.vixio.util.Util;
 import me.iblitzkriegi.vixio.util.skript.EasyMultiple;
 import me.iblitzkriegi.vixio.util.wrapper.Bot;
 import net.dv8tion.jda.core.entities.Guild;
 import org.bukkit.event.Event;
 
-public class ExprGuilds extends PropertyExpression<Bot, Guild> implements EasyMultiple<Bot, Guild> {
+import java.util.Arrays;
+
+public class ExprGuilds extends SimpleExpression<Guild> implements EasyMultiple<Bot, Guild> {
     static {
         Vixio.getInstance().registerPropertyExpression(ExprGuilds.class, Guild.class,
-                "guild", "bots")
+                "guild", "bot")
                 .setName("Guilds of bot")
                 .setDesc("Get all the guilds of a bot")
                 .setExample("set {var::*} to guilds of event-bot");
     }
 
+    private Expression<Object> bot;
+
     @Override
-    protected Guild[] get(Event e, Bot[] source) {
-        return convert(getReturnType(), getExpr().getAll(e), bot -> bot.getJDA().getGuilds().stream()
-                .toArray(size -> new Guild[size])
-        );
+    protected Guild[] get(Event e) {
+        Bot bot = Util.botFrom(this.bot.getSingle(e));
+        if (bot != null) {
+            return bot.getJDA().getGuilds().toArray(new Guild[0]);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean isSingle() {
+        return false;
     }
 
     @Override
@@ -33,12 +45,12 @@ public class ExprGuilds extends PropertyExpression<Bot, Guild> implements EasyMu
 
     @Override
     public String toString(Event e, boolean debug) {
-        return "the guilds of " + getExpr().toString(e, debug);
+        return bot.toString(e, debug) + "'s guilds";
     }
 
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
-        setExpr((Expression<Bot>) exprs[0]);
+        bot = (Expression<Object>) exprs[0];
         return true;
     }
 }
