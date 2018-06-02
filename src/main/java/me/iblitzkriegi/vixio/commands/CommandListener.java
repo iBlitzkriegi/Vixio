a79b6ce4c72f3a095e9e89dead0f0eeb84b5346d
iBlitzkriegi <Blitz>
2018-06-01 20:59:07 -0400

    Add event field to use for user parsing

--- src/main/java/me/iblitzkriegi/vixio/commands/CommandListener.java
+++ src/main/java/me/iblitzkriegi/vixio/commands/CommandListener.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 
 public class CommandListener extends ListenerAdapter {
+    public static MessageReceivedEvent lastCommandEvent;
 
 
     @Override
@@ -21,27 +22,29 @@ public class CommandListener extends ListenerAdapter {
 
         for (DiscordCommand command : DiscordCommandFactory.getInstance().getCommands()) {
 
-			for (Expression<String> prefix : command.getPrefixes()) {
+            for (Expression<String> prefix : command.getPrefixes()) {
 
                 for (String alias : command.getUsableAliases()) {
 
-					DiscordCommandEvent event = new DiscordCommandEvent(null, alias, command, null,
-							e.getGuild(), e.getChannel(), e.getTextChannel(), e.getMessage(),
-							e.getAuthor(), e.getMember(), Util.botFromID(e.getJDA().getSelfUser().getId()));
-					String rawPrefix = prefix.getSingle(event);
-					if (nonNull(rawPrefix) && StringUtils.startsWithIgnoreCase(content, rawPrefix + alias)) {
-						event.setPrefix(rawPrefix);
-						try {
-							event.setArguments(content.substring((rawPrefix + alias).length() + 1));
-						} catch (StringIndexOutOfBoundsException e1) {
-							event.setArguments(null);
-						}
-						// Because most of bukkit's apis are sync only, make sure to run this on bukkit's thread
-						Util.sync(() -> {
+                    DiscordCommandEvent event = new DiscordCommandEvent(null, alias, command, null,
+                            e.getGuild(), e.getChannel(), e.getTextChannel(), e.getMessage(),
+                            e.getAuthor(), e.getMember(), Util.botFromID(e.getJDA().getSelfUser().getId()));
+                    String rawPrefix = prefix.getSingle(event);
+                    if (nonNull(rawPrefix) && StringUtils.startsWithIgnoreCase(content, rawPrefix + alias)) {
+                        event.setPrefix(rawPrefix);
+                        try {
+                            event.setArguments(content.substring((rawPrefix + alias).length() + 1));
+                        } catch (StringIndexOutOfBoundsException e1) {
+                            event.setArguments(null);
+                        }
+                        // Because most of bukkit's apis are sync only, make sure to run this on bukkit's thread
+                        Util.sync(() -> {
+
+                            lastCommandEvent = e;
 
                             Bukkit.getPluginManager().callEvent(event);
                             if (!event.isCancelled()) {
-								command.execute(event);
+                                command.execute(event);
                             }
 
                         });
@@ -57,19 +60,19 @@ public class CommandListener extends ListenerAdapter {
         }
     }
 
-	/**
-	 * Checks if a string is null or the localized string form of null in Skript (usually <none>)
-	 */
-	public boolean nonNull(String s) {
-		if (s == null) {
-			return false;
-		}
-		String localized = Language.get("none");
-		if (localized == null) {
-			// on old skript versions you couldn't change <none> so assume that if result is null
-			return !s.equals("<none>");
-		}
-		return !s.equals(localized);
-	}
+    /**
+     * Checks if a string is null or the localized string form of null in Skript (usually <none>)
+     */
+    public boolean nonNull(String s) {
+        if (s == null) {
+            return false;
+        }
+        String localized = Language.get("none");
+        if (localized == null) {
+            // on old skript versions you couldn't change <none> so assume that if result is null
+            return !s.equals("<none>");
+        }
+        return !s.equals(localized);
+    }
 
 }
