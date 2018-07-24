ba143155a57a38d5a990e0be9ce11f7286e1e551
iBlitzkriegi <Blitz>
2018-07-23 20:17:13 -0400

    Fixed the member has role condition
    Simplified completley, removes the conflict with skript's conflict condition, and allows direct checking by name without having to use the role named expression. closes #49

--- src/main/java/me/iblitzkriegi/vixio/conditions/CondMemberRole.java
+++ src/main/java/me/iblitzkriegi/vixio/conditions/CondMemberRole.java
@@ -12,36 +12,55 @@ import org.bukkit.event.Event;
 public class CondMemberRole extends Condition {
     static {
         Vixio.getInstance().registerCondition(CondMemberRole.class,
-                "%member% has %role%", "%member% (does[n[']t]|does not) have %role%")
+                "%member% (has|have) [the] role %role%", "%member% (does[n[']t]|does not) have [the] role %role%",
+                "%member% (has|have) [the] role [named] %string%", "%member% (does[n[']t]|does not) have [the] role [named] %string%")
                 .setName("Member has role")
-                .setDesc("Check if a member has a specific role")
-                .setExample("if event-member has role with id \"216516161651\"");
+                .setDesc("Check if a member either does, or does not have either a specific %role$ or a role with a certain name.")
+                .setExample(
+                        "discord command $role <member> <string>:",
+                        "\ttrigger:",
+                        "\t\tif arg-1 has role named arg-2:",
+                        "\t\t\treply with \"%arg-1% does have a role named %arg-2%\"",
+                        "\t\t\tstop",
+                        "\t\treply with \"%arg-1% does not have a role named %arg-2%\""
+                );
     }
 
     private Expression<Member> member;
-    private Expression<Role> role;
+    private Expression<Object> role;
     private boolean not;
+    private boolean roleName;
 
     @Override
     public boolean check(Event e) {
         Member member = this.member.getSingle(e);
-        Role role = this.role.getSingle(e);
+        Object role = this.role.getSingle(e);
         if (role == null || member == null) {
             return false;
+        }
+        if (roleName) {
+            String name = (String) role;
+            for (Role role2 : member.getRoles()) {
+                if (role2.getName().equalsIgnoreCase(name)) {
+                    return not == true;
+                }
+            }
+            return not == false;
         }
         return not == member.getRoles().contains(role);
     }
 
     @Override
     public String toString(Event e, boolean debug) {
-        return member.toString(e, debug) + (not ? " has " : " does not have ") + role.toString(e, debug);
+        return member.toString(e, debug) + (not ? " has " : " does not have ") + (roleName ? "the role named " : "") + role.toString(e, debug);
     }
 
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
         member = (Expression<Member>) exprs[0];
-        role = (Expression<Role>) exprs[1];
-        not = matchedPattern == 0;
+        role = (Expression<Object>) exprs[1];
+        roleName = matchedPattern == 2 || matchedPattern == 3;
+        not = matchedPattern == 0 || matchedPattern == 2;
         return true;
     }
 }
