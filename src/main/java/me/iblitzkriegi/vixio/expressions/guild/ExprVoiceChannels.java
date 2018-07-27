a1f23689d8f66fa79711e7e33ee6239556f81f47
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2018-07-27 12:57:56 -0400

    Revert "Merged members voice channel and guilds voice channels"
    This reverts commit 9e3c7e21d77aeef6c0fd9fc87bc9350f5dc34490.

--- src/main/java/me/iblitzkriegi/vixio/expressions/guild/ExprVoiceChannels.java
+++ src/main/java/me/iblitzkriegi/vixio/expressions/guild/ExprVoiceChannels.java
@@ -5,7 +5,9 @@ import ch.njol.skript.lang.SkriptParser;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
 import me.iblitzkriegi.vixio.Vixio;
-import net.dv8tion.jda.core.entities.*;
+import net.dv8tion.jda.core.entities.Category;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.VoiceChannel;
 import org.bukkit.event.Event;
 
 import java.util.List;
@@ -13,18 +15,18 @@ import java.util.List;
 public class ExprVoiceChannels extends SimpleExpression<VoiceChannel> {
     static {
         Vixio.getInstance().registerPropertyExpression(ExprVoiceChannels.class, VoiceChannel.class,
-                "voice(-| )channel", "guild/category/member")
-                .setName("Voice Channels of")
-                .setDesc("Get all of the voice channels in a guild or category, or the voice channel a member is in.")
+                "voice(-| )channel", "guild/category")
+                .setName("Voice Channels")
+                .setDesc("Get all of the voice channels of a guild or category.")
                 .setExample(
-                        "discord command $voice:",
-                        "\ttrigger:",
-                        "\t\treply with \"Here are the voice channels you can join! `%voice channels of event-guild%`\""
+                        "on guild message receive:",
+                        "\tset {_channels::*} to voice channels of event-guild",
+                        "\tloop {_channels::*}:",
+                        "\t\tbroadcast \"%name of loop-value%\""
                 );
     }
 
     private Expression<Object> object;
-    private boolean single;
 
     @Override
     protected VoiceChannel[] get(Event e) {
@@ -35,16 +37,13 @@ public class ExprVoiceChannels extends SimpleExpression<VoiceChannel> {
         } else if (object instanceof Category) {
             List<VoiceChannel> channels = ((Category) object).getVoiceChannels();
             return ((Guild) object).getVoiceChannels().toArray(new VoiceChannel[channels.size()]);
-        } else if (object instanceof Member) {
-            GuildVoiceState state = ((Member)object).getVoiceState();
-           return state.inVoiceChannel() ? new VoiceChannel[]{state.getChannel()} : null;
         }
         return null;
     }
 
     @Override
     public boolean isSingle() {
-        return single;
+        return false;
     }
 
     @Override
@@ -60,7 +59,6 @@ public class ExprVoiceChannels extends SimpleExpression<VoiceChannel> {
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
         object = (Expression<Object>) exprs[0];
-        single = exprs[0].getReturnType().isAssignableFrom(Member.class) && exprs[0].isSingle();
         return true;
     }
 }
