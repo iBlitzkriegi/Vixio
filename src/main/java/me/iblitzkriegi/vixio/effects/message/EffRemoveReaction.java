a1c8365ed2bacc6f7823f4dc544df1ee7595a961
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2018-08-31 17:46:15 -0400

    Allow for removing multiple emotes added by a user
    You may now remove multiple emotes added by a person on a message, test skript:
    ```
    discord command $emotes:
    	trigger:
    		add emotes "one" and "two" to event-message
    		wait a second
    		remove emotes "one" and "two" added by event-bot from event-message
    ```
    closes #83

--- src/main/java/me/iblitzkriegi/vixio/effects/message/EffRemoveReaction.java
+++ src/main/java/me/iblitzkriegi/vixio/effects/message/EffRemoveReaction.java
@@ -10,16 +10,14 @@ import me.iblitzkriegi.vixio.util.skript.AsyncEffect;
 import me.iblitzkriegi.vixio.util.wrapper.Bot;
 import me.iblitzkriegi.vixio.util.wrapper.Emote;
 import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.entities.MessageReaction;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.exceptions.PermissionException;
-import net.dv8tion.jda.core.exceptions.RateLimitedException;
 import org.bukkit.event.Event;
 
 public class EffRemoveReaction extends AsyncEffect {
     static {
-        Vixio.getInstance().registerEffect(EffRemoveReaction.class, "remove %emote% added by %user% from %message% [with %bot/string%]")
+        Vixio.getInstance().registerEffect(EffRemoveReaction.class, "remove %emotes% added by %user% from %message% [with %bot/string%]")
                 .setName("Remove Emote by User")
                 .setDesc("Remove a specific users emote from a message, this is for removing a users reacted emote in the reaction add event mostly.")
                 .setExample(
@@ -35,11 +33,10 @@ public class EffRemoveReaction extends AsyncEffect {
 
     @Override
     protected void execute(Event e) {
-        Emote emote = this.emote.getSingle(e);
         User user = this.user.getSingle(e);
         Message message = UpdatingMessage.convert(this.message.getSingle(e));
         Bot bot = Util.botFrom(this.bot.getSingle(e));
-        if (emote == null || user == null || message == null || bot == null) {
+        if (user == null || message == null || bot == null) {
             return;
         }
         Util.bindMessage(bot, message).queue(bindedMessage -> {
@@ -47,11 +44,13 @@ public class EffRemoveReaction extends AsyncEffect {
                 return;
             }
             for (MessageReaction messageReaction : bindedMessage.getReactions()) {
-                if (messageReaction.getReactionEmote().getName().equalsIgnoreCase(emote.getName())) {
-                    try {
-                        messageReaction.removeReaction(user).queue();
-                    } catch (PermissionException x) {
-                        Vixio.getErrorHandler().needsPerm(bot, "remove reaction from user", x.getPermission().getName());
+                for (Emote emote : this.emote.getAll(e)) {
+                    if (messageReaction.getReactionEmote().getName().equalsIgnoreCase(emote.getName())) {
+                        try {
+                            messageReaction.removeReaction(user).queue();
+                        } catch (PermissionException x) {
+                            Vixio.getErrorHandler().needsPerm(bot, "remove reaction from user", x.getPermission().getName());
+                        }
                     }
                 }
             }
