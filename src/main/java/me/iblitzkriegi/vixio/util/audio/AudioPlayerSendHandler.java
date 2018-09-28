e6a675c144988e123ac60abd9d74819aee61c03d
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2018-09-28 19:28:53 -0400

    Update JDA and audio handler

--- src/main/java/me/iblitzkriegi/vixio/util/audio/AudioPlayerSendHandler.java
+++ src/main/java/me/iblitzkriegi/vixio/util/audio/AudioPlayerSendHandler.java
@@ -7,29 +7,20 @@ import net.dv8tion.jda.core.audio.AudioSendHandler;
 public class AudioPlayerSendHandler implements AudioSendHandler {
     private final AudioPlayer audioPlayer;
     private AudioFrame lastFrame;
+
     public AudioPlayerSendHandler(AudioPlayer audioPlayer) {
         this.audioPlayer = audioPlayer;
     }
 
     @Override
     public boolean canProvide() {
-        if (lastFrame == null) {
-            lastFrame = audioPlayer.provide();
-        }
-
+        lastFrame = audioPlayer.provide();
         return lastFrame != null;
     }
 
     @Override
     public byte[] provide20MsAudio() {
-        if (lastFrame == null) {
-            lastFrame = audioPlayer.provide();
-        }
-
-        byte[] data = lastFrame != null ? lastFrame.data : null;
-        lastFrame = null;
-
-        return data;
+        return lastFrame.getData();
     }
 
     @Override
