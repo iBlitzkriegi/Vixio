00ac19900a688d4e3b755cb5862bfc04d1f01b11
BuildTools <alexlew95@gmail.com>
2019-01-19 23:14:29 +0100

    Vixio doc first push

---
+++ js/index.js
@@ -0,0 +1,32 @@
+fetch("./Syntaxes.txt")
+	.then(function(response) {
+		return response.text();
+	})
+	.then(function(syntaxes) {
+		let lines = syntaxes.split("\n")
+		let type = lines[0];
+		for (line in lines) {
+			
+		}
+	});
+
+const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+// Check if there are any navbar burgers
+if ($navbarBurgers.length > 0) {
+
+// Add a click event on each of them
+$navbarBurgers.forEach( el => {
+el.addEventListener('click', () => {
+
+  // Get the target from the "data-target" attribute
+  const target = el.dataset.target;
+  const $target = document.getElementById(target);
+
+  // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+  el.classList.toggle('is-active');
+  $target.classList.toggle('is-active');
+
+});
+});
+};
