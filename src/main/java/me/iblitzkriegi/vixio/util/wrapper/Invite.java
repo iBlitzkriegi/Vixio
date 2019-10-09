d1c2780335458a10053208050982067ed584ddec
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2019-10-08 22:31:06 -0400

    Create invite wrapper for invite generation

---
+++ src/main/java/me/iblitzkriegi/vixio/util/wrapper/Invite.java
@@ -0,0 +1,89 @@
+package me.iblitzkriegi.vixio.util.wrapper;
+
+import net.dv8tion.jda.api.entities.GuildChannel;
+import net.dv8tion.jda.api.entities.User;
+
+public class Invite {
+
+    private int maxUses;
+    private int maxAge;
+    private boolean temporary;
+    private boolean unique;
+    private GuildChannel channel;
+    private boolean guild;
+    private User inviter;
+    private String url;
+    private String code;
+
+    public int getMaxUses() {
+        return maxUses;
+    }
+
+    public void setMaxUses(int maxUses) {
+        this.maxUses = maxUses;
+    }
+
+    public int getMaxAge() {
+        return maxAge;
+    }
+
+    public void setMaxAge(int maxAge) {
+        this.maxAge = maxAge;
+    }
+
+    public boolean isTemporary() {
+        return temporary;
+    }
+
+    public void setTemporary(boolean temporary) {
+        this.temporary = temporary;
+    }
+
+    public boolean isUnique() {
+        return unique;
+    }
+
+    public void setUnique(boolean unique) {
+        this.unique = unique;
+    }
+
+    public GuildChannel getChannel() {
+        return channel;
+    }
+
+    public void setChannel(GuildChannel channel) {
+        this.channel = channel;
+    }
+
+    public boolean isGuild() {
+        return guild;
+    }
+
+    public void setGuild(boolean guild) {
+        this.guild = guild;
+    }
+
+    public User getInviter() {
+        return inviter;
+    }
+
+    public void setInviter(User inviter) {
+        this.inviter = inviter;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
