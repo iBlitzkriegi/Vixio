da78581414723335aea0ee33431c1b2c2a63e377
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2020-02-15 16:22:34 -0500

    Rewrite this because I couldn't get it to work

--- src/main/java/me/iblitzkriegi/vixio/events/member/EvtRemoveRole.java
+++ src/main/java/me/iblitzkriegi/vixio/events/member/EvtRemoveRole.java
@@ -13,19 +13,32 @@ import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRoleRemoveEvent;
 
 public class EvtRemoveRole extends BaseEvent<GuildMemberRoleRemoveEvent> {
-
     static {
 
-        BaseEvent.register("member role remove", EvtRemoveRole.class, RoleRemoveEvent.class,
+        BaseEvent.register("member remove role", EvtRemoveRole.class, RoleRemoveEvent.class,
                 "member role remove[d]")
                 .setName("Role Removed")
-                .setDesc("Fired when a role is removed from a member.")
+                .setDesc("Fired when a Member has a role or roles removed from them. You can use the `the roles` expression to get all the roles.")
                 .setExample("on member role remove:");
 
         EventValues.registerEventValue(RoleRemoveEvent.class, Bot.class, new Getter<Bot, RoleRemoveEvent>() {
             @Override
             public Bot get(RoleRemoveEvent event) {
                 return Util.botFrom(event.getJDAEvent().getJDA());
+            }
+        }, 0);
+
+        EventValues.registerEventValue(RoleRemoveEvent.class, Guild.class, new Getter<Guild, RoleRemoveEvent>() {
+            @Override
+            public Guild get(RoleRemoveEvent event) {
+                return event.getJDAEvent().getGuild();
+            }
+
+        }, 0);
+        EventValues.registerEventValue(RoleRemoveEvent.class, Role.class, new Getter<Role, RoleRemoveEvent>() {
+            @Override
+            public Role get(RoleRemoveEvent event) {
+                return event.getJDAEvent().getRoles().get(0);
             }
         }, 0);
 
@@ -43,22 +56,9 @@ public class EvtRemoveRole extends BaseEvent<GuildMemberRoleRemoveEvent> {
             }
         }, 0);
 
-        EventValues.registerEventValue(RoleRemoveEvent.class, Guild.class, new Getter<Guild, RoleRemoveEvent>() {
-            @Override
-            public Guild get(RoleRemoveEvent event) {
-                return event.getJDAEvent().getGuild();
-            }
-        }, 0);
-
-        EventValues.registerEventValue(RoleRemoveEvent.class, Role.class, new Getter<Role, RoleRemoveEvent>() {
-            @Override
-            public Role get(RoleRemoveEvent event) {
-                return event.getJDAEvent().getRoles().get(0);
-            }
-        }, 0);
     }
 
-    public static class RoleRemoveEvent extends SimpleVixioEvent<GuildMemberRoleRemoveEvent> {
+    public class RoleRemoveEvent extends SimpleVixioEvent<GuildMemberRoleRemoveEvent> {
     }
 
 }
