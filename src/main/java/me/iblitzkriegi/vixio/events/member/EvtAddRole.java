69cb8025905be4d943f91d3f6e500d80f85eda25
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2020-02-15 16:10:42 -0500

    Re-write this because I couldn't get it to work

--- src/main/java/me/iblitzkriegi/vixio/events/member/EvtAddRole.java
+++ src/main/java/me/iblitzkriegi/vixio/events/member/EvtAddRole.java
@@ -13,19 +13,32 @@ import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRoleAddEvent;
 
 public class EvtAddRole extends BaseEvent<GuildMemberRoleAddEvent> {
-
     static {
 
-        BaseEvent.register("member role add", EvtAddRole.class, RoleAddEvent.class,
+        BaseEvent.register("member add role", EvtAddRole.class, RoleAddEvent.class,
                 "member role add[ed]")
                 .setName("Role Added")
-                .setDesc("Fired when a role is added to a member.")
+                .setDesc("Fired when a Member receives a new role or roles. You can use the `the roles` expression to get all the roles.")
                 .setExample("on member role add:");
 
         EventValues.registerEventValue(RoleAddEvent.class, Bot.class, new Getter<Bot, RoleAddEvent>() {
             @Override
             public Bot get(RoleAddEvent event) {
                 return Util.botFrom(event.getJDAEvent().getJDA());
+            }
+        }, 0);
+
+        EventValues.registerEventValue(RoleAddEvent.class, Guild.class, new Getter<Guild, RoleAddEvent>() {
+            @Override
+            public Guild get(RoleAddEvent event) {
+                return event.getJDAEvent().getGuild();
+            }
+
+        }, 0);
+        EventValues.registerEventValue(RoleAddEvent.class, Role.class, new Getter<Role, RoleAddEvent>() {
+            @Override
+            public Role get(RoleAddEvent event) {
+                return event.getJDAEvent().getRoles().get(0);
             }
         }, 0);
 
@@ -43,22 +56,9 @@ public class EvtAddRole extends BaseEvent<GuildMemberRoleAddEvent> {
             }
         }, 0);
 
-        EventValues.registerEventValue(RoleAddEvent.class, Guild.class, new Getter<Guild, RoleAddEvent>() {
-            @Override
-            public Guild get(RoleAddEvent event) {
-                return event.getJDAEvent().getGuild();
-            }
-        }, 0);
-
-        EventValues.registerEventValue(RoleAddEvent.class, Role.class, new Getter<Role, RoleAddEvent>() {
-            @Override
-            public Role get(RoleAddEvent event) {
-                return event.getJDAEvent().getRoles().get(0);
-            }
-        }, 0);
     }
 
-    public static class RoleAddEvent extends SimpleVixioEvent<GuildMemberRoleAddEvent> {
+    public class RoleAddEvent extends SimpleVixioEvent<GuildMemberRoleAddEvent> {
     }
 
 }
