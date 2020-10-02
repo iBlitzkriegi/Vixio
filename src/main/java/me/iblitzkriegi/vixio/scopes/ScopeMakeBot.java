ef077613e308eec3e39a749d0d9213e6c028cbf0
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2020-10-02 13:59:28 -0400

    Convert to DefaultShardManager

--- src/main/java/me/iblitzkriegi/vixio/scopes/ScopeMakeBot.java
+++ src/main/java/me/iblitzkriegi/vixio/scopes/ScopeMakeBot.java
@@ -7,12 +7,13 @@ import me.iblitzkriegi.vixio.Vixio;
 import me.iblitzkriegi.vixio.util.scope.EffectSection;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import org.bukkit.event.Event;
 
 public class ScopeMakeBot extends EffectSection {
 
     public static JDA bot;
-    public static JDABuilder jdaBuilder;
+    public static DefaultShardManagerBuilder jdaBuilder;
 
     static {
         Vixio.getInstance().registerCondition(ScopeMakeBot.class, "(make|create) vixio bot")
@@ -29,7 +30,7 @@ public class ScopeMakeBot extends EffectSection {
 
     @Override
     protected void execute(Event e) {
-        jdaBuilder = JDABuilder.createDefault("temporary token");
+        jdaBuilder = DefaultShardManagerBuilder.createDefault("temporary token");
         runSection(e);
     }
 
